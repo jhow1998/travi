2e84cbbc6f45cf5c9e19eb36ced18421c578b179
joseph <joseph.robert.carvalho@gmail.com>
2023-01-09 00:07:53 -0300

    completo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+AOS.init({
+    duration:400,
+    delay:200,
+});
